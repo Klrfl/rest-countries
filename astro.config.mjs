5347381d652fcc7530897a978e2250f8bc7b4817
Efraim Munthe <efrayanglain@gmail.com>
2023-12-31 17:54:20 +0700

    make some components (still basically a mess)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  site: "https://klrfl-countries.netlify.app",
+});
