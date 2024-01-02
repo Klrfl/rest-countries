cde527f13de3fead72963c2fc4c56ca7c50d48aa
Efraim Munthe <efrayanglain@gmail.com>
2024-01-02 12:04:10 +0700

    implement search functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   site: "https://klrfl-countries.netlify.app",
 });
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -4,7 +4,13 @@ export function slugify(string) {
     .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
     .trim() // trim leading or trailing whitespace
     .toLowerCase() // convert to lowercase
-    .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
     .replace(/\s+/g, "-") // replace spaces with hyphens
     .replace(/-+/g, "-"); // remove consecutive hyphens
+}
+
+export function normalize(string) {
+  return String(string)
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase(); // remove all the accents, which happen to be all in the \u03xx UNICODE block.
 }
