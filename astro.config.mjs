8c1c1df01e221f2c20df87aec4ae97c697484dae
Efraim Munthe <efrayanglain@gmail.com>
2024-01-02 14:23:02 +0700

    add netlify SSR adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   site: "https://klrfl-countries.netlify.app",
+  adapter: netlify(),
 });
+
